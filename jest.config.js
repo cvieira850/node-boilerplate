88484ef30b839b61f2fd01dc4c8230c3eb6b174a
Caio Vieira <contato@caiovieira.com.br>
2021-03-25 21:18:55 -0300

    chore: add jest config and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/infra/validators/**',
+    '!<rootDir>/src/infra/bd/postgresql/typeorm/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
