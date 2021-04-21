5c41117bc87a31c359b6ce7af03ce9fe7da66ed1
Caio Vieira <contato@caiovieira.com.br>
2021-04-21 15:22:57 -0300

    refactor: move httpHelper to a folder
    issue: #6

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse,Authentication,Validation } from './login-protocols'
-import { badRequest, ok, serverError, unaunthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unaunthorized } from '../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
   constructor (
