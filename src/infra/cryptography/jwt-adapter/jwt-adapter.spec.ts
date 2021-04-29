1a148059356a514962b1ce19843741da8b141bd4
Caio Vieira <contato@caiovieira.com.br>
2021-04-28 21:27:42 -0300

    test: ensure JwtAdapter throws if sign throws
    issue: #10

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,16 +8,25 @@ jest.mock('jsonwebtoken', () => ({
 }))
 
 describe('Jwt Adapter', () => {
-  test('should call sign with correct values', async () => {
+  test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt,'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
-  test('should return a token on sign success', async () => {
+  test('Should return a token on sign success', async () => {
     const sut = new JwtAdapter('secret')
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
+  })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt,'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_id')
+    await expect(promise).rejects.toThrow()
   })
 })
