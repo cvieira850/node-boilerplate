244f6c652c50295b10d6c0951cb6353e2fb0923e
Caio Vieira <contato@caiovieira.com.br>
2021-04-29 20:20:13 -0300

    feat: ensure AccountPgRepository return an account on loadByEmail Success
    issue #10

--- src/infra/db/postgresql/account-repository/account.ts
+++ src/infra/db/postgresql/account-repository/account.ts
@@ -1,15 +1,22 @@
 
 import { getRepository } from 'typeorm'
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import User from '../typeorm/entities/user'
 
-export class AccountPgRepository implements AddAccountRepository {
+export class AccountPgRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const userRepository = getRepository(User)
     const userCreated = userRepository.create(accountData)
     const account = await userRepository.save(userCreated)
+    return account
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const userRepository = getRepository(User)
+    const account = userRepository.findOne({ email })
     return account
   }
 }
