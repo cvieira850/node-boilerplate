9e74fbc8b5d363a45861eaded4af0be3a04f364e
Caio Vieira <contato@caiovieira.com.br>
2021-05-01 18:54:32 -0300

    test: add login route
    issue: #12

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,8 @@
+import { hash } from 'bcrypt'
 import request from 'supertest'
 import { Connection, createConnection, getConnection } from 'typeorm'
 import { tables } from '../../infra/db/pg/tables'
+import User from '../../infra/db/pg/typeorm/entities/user'
 import app from '../config/app'
 
 let connection: Connection
@@ -37,6 +39,30 @@ describe('Login Routes', () => {
           email: 'contato@caiovieira.com.br',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123',12)
+      await getConnection()
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(
+          {
+            name: 'valid_name',
+            email: 'contato@caiovieira.com.br',
+            password
+          }
+        )
+        .execute()
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'contato@caiovieira.com.br',
+          password: '123'
         })
         .expect(200)
     })
