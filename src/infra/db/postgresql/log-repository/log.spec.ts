e0cea34b0a4075aafcb59585804c4a5c414a1e7d
Caio Vieira <contato@caiovieira.com.br>
2021-05-01 08:24:06 -0300

    refactor: change test to resolve warning
    issue: #12

--- src/infra/db/postgresql/log-repository/log.spec.ts
+++ src/infra/db/postgresql/log-repository/log.spec.ts
@@ -37,6 +37,7 @@ describe('Log Pg Repository', () => {
   test('Should create an error log on success', async () => {
     const sut = makeSut()
     await sut.logError('any_error')
+    jest.setTimeout(30000)
     const ErrorRepository = getRepository(Error)
     const count = await ErrorRepository.count()
     expect(count).toBe(1)
