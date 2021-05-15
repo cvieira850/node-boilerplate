95b5ecb6f86cc1564222656744b88636228b5578
Caio Vieira <contato@caiovieira.com.br>
2021-05-15 17:28:02 -0300

    test: ensure AddRoleController returns a badRequest error if validation fails
    issue: #32

--- src/presentation/controllers/role/add-role/add-role-controller.spec.ts
+++ src/presentation/controllers/role/add-role/add-role-controller.spec.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, Validation } from './add-role-controller-protocols'
 import { AddRoleController } from './add-role-controller'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -37,5 +38,12 @@ describe('AddRoleController', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub,'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
