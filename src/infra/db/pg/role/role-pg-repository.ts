1314f4b2085fa68287602783fbc2f16a290c0c6e
Caio Vieira <contato@caiovieira.com.br>
2021-05-16 09:05:21 -0300

    feat: ensure RolePgRepository returns a role on loadByName success
    issue: #36

--- src/infra/db/pg/role/role-pg-repository.ts
+++ src/infra/db/pg/role/role-pg-repository.ts
@@ -1,16 +1,23 @@
 
 import { AddRoleRepository } from '../../../../data/protocols/db/role/add-role-repository'
+import { LoadRoleByNameRepository } from '../../../../data/usecases/add-role/db-add-role-protocols'
 import { AddRoleModel } from '../../../../domain/usecases/add-role'
 import { RoleModel } from '../../../../domain/models/role'
 import Role from '../typeorm/entities/role'
 import { getRepository } from 'typeorm'
 
 export class RolePgRepository implements
-AddRoleRepository {
+AddRoleRepository, LoadRoleByNameRepository {
   async add (roleData: AddRoleModel): Promise<RoleModel> {
     const roleRepository = getRepository(Role)
     const roleCreated = roleRepository.create(roleData)
     const role = await roleRepository.save(roleCreated)
+    return role
+  }
+
+  async loadByName (name: string): Promise<RoleModel> {
+    const userRepository = getRepository(Role)
+    const role = await userRepository.findOne({ name })
     return role
   }
 }
