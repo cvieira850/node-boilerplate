e9e66967db513a1eca8d966870ecae8ba8082d2b
Caio Vieira <contato@caiovieira.com.br>
2021-05-17 20:51:52 -0300

    feat: ensure AddRoleToUserController returns 400 if Validation fails
    issue: #43

--- src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.ts
+++ src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.ts
@@ -1,9 +1,10 @@
+import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-role-to-user-protocols'
 
 export class AddRoleToUserController implements Controller {
   constructor (private readonly validation: Validation) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
-    return new Promise(resolve => resolve(null))
+    const error = this.validation.validate(httpRequest.body)
+    return new Promise(resolve => resolve(badRequest(error)))
   }
 }
