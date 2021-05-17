b293826dd6beabeba26c8d9a8e873af13ebcdadc
Caio Vieira <contato@caiovieira.com.br>
2021-05-17 20:52:11 -0300

    test: ensure AddRoleToUserController returns 400 if Validation fails
    issue: #43

--- src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.spec.ts
+++ src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.spec.ts
@@ -1,5 +1,6 @@
 import { Validation } from './add-role-to-user-protocols'
 import { AddRoleToUserController } from './add-role-to-user-controller'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
@@ -36,5 +37,18 @@ describe('AddRoleToUserController', () => {
     }
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub,'validate').mockReturnValueOnce(new Error())
+    const httpRequest = {
+      body: {
+        userId: 'any_user_id',
+        roleId: 'any_role_id'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
