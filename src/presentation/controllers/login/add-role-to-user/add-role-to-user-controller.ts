0454262d23a2d6ee52f7ed0c39f6f7fe2e2abc1d
Caio Vieira <contato@caiovieira.com.br>
2021-05-18 09:14:48 -0300

    feat: ensure AddRoleToUserController returns 500 if AddRoleToUser UseCase throws
    issue: #43

--- src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.ts
+++ src/presentation/controllers/login/add-role-to-user/add-role-to-user-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Validation, AddRoleToUser } from './add-role-to-user-protocols'
-import { badRequest, forbidden } from '../../../helpers/http/http-helper'
+import { badRequest, forbidden, serverError } from '../../../helpers/http/http-helper'
 import { InvalidRoleOrUserError } from '../../../errors'
 
 export class AddRoleToUserController implements Controller {
@@ -9,13 +9,17 @@ export class AddRoleToUserController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
-    }
-    const user = await this.addRoleToUser.addRoleToUser(httpRequest.body)
-    if (!user) {
-      return forbidden(new InvalidRoleOrUserError())
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const user = await this.addRoleToUser.addRoleToUser(httpRequest.body)
+      if (!user) {
+        return forbidden(new InvalidRoleOrUserError())
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
