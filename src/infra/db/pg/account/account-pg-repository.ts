08c455e58755e7df9e51bfc3e1776145853dc013
Caio Vieira <contato@caiovieira.com.br>
2021-05-20 11:33:54 -0300

    feat: ensure accountPgRepository returns an account on addRoleToUser success
    issue: #48

--- src/infra/db/pg/account/account-pg-repository.ts
+++ src/infra/db/pg/account/account-pg-repository.ts
@@ -1,18 +1,23 @@
 
-import { LoadAccountByIdRepository } from '../../../../data/usecases/add-role-to-user/db-add-role-to-user-protocols'
 import { getRepository } from 'typeorm'
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
+import { AddRoleToUserRepository } from '../../../../data/protocols/db/account/add-role-to-user-repository'
+import { LoadAccountByIdRepository } from '../../../../data/protocols/db/account/load-account-by-id-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { AddRoleToUserModel } from '../../../../domain/usecases/add-role-to-user'
+
 import User from '../typeorm/entities/user'
+import Role from '../typeorm/entities/role'
 
 export class AccountPgRepository implements
 AddAccountRepository,
 LoadAccountByEmailRepository,
 UpdateAccessTokenRepository,
-LoadAccountByIdRepository {
+LoadAccountByIdRepository,
+AddRoleToUserRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const userRepository = getRepository(User)
     const userCreated = userRepository.create(accountData)
@@ -43,5 +48,18 @@ LoadAccountByIdRepository {
       return account
     }
     return null
+  }
+
+  async addRoleToUser (data: AddRoleToUserModel): Promise<AccountModel> {
+    const { userId, roleId } = data
+    const UserRepository = getRepository(User)
+    const RoleRepository = getRepository(Role)
+    const account = await UserRepository.findOne(userId)
+    const role = await RoleRepository.findOne(roleId)
+    account.role_id = roleId
+    account.role = role
+    const updatedAccount = account
+    const updatedAccountResult = await UserRepository.save(updatedAccount)
+    return updatedAccountResult
   }
 }
