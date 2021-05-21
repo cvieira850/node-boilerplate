b0c5dca4dc47e38890e7c7e51d517c7ee04f7a52
Caio Vieira <contato@caiovieira.com.br>
2021-05-20 21:48:53 -0300

    feat: ensure DbLoadAccountByToken calls LoadRoleByIdRepository with correct value
    issue: #55

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,19 +1,26 @@
 import { AccountModel } from '../../../domain/models/account'
 import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
 import { Decrypter } from '../../protocols/cryptography/decrypter'
+import { LoadRoleByIdRepository } from '../../protocols/db/role/load-role-by-id-repository'
 import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (
     private readonly decrypter: Decrypter,
-    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
+    private readonly loadRoleByIdRepository: LoadRoleByIdRepository
   ) {}
 
   async loadByToken (accessToken: string, role?: string): Promise<AccountModel> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
       const account = await this.loadAccountByTokenRepository.loadByToken(token)
-      return account
+      if (account) {
+        if (role) {
+          await this.loadRoleByIdRepository.loadById(account.role_id)
+        }
+        return account
+      }
     }
     return null
   }
