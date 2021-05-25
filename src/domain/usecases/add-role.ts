24d6ae65234d27ab56aeee79c19a981bdce6c6c1
Caio Vieira <contato@caiovieira.com.br>
2021-05-25 09:30:31 -0300

    refactor: change models to use Omit
    issue: #69

--- src/domain/usecases/add-role.ts
+++ src/domain/usecases/add-role.ts
@@ -1,8 +1,6 @@
 import { RoleModel } from '@/domain/models/role'
 
-export type AddRoleModel = {
-  name: string
-}
+export type AddRoleModel = Omit<RoleModel, 'id'>
 
 export interface AddRole {
   add: (data: AddRoleModel) => Promise<RoleModel>
