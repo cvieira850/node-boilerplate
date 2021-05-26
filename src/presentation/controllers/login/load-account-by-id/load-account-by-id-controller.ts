7c67f7136296c4d07fc769e0114a7099b0b791f5
Caio Vieira <contato@caiovieira.com.br>
2021-05-26 20:10:48 -0300

    feat: ensure LoadAccountByIdController throw if LoadAccountById throws
    issue: #72

--- src/presentation/controllers/login/load-account-by-id/load-account-by-id-controller.ts
+++ src/presentation/controllers/login/load-account-by-id/load-account-by-id-controller.ts
@@ -1,14 +1,18 @@
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadAccountById } from './load-account-by-id-protocols'
 
 export class LoadAccountByIdController implements Controller {
   constructor (private readonly loadAccountById: LoadAccountById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const user = await this.loadAccountById.loadById(httpRequest.params.userId)
-    if (!user) {
-      return forbidden(new InvalidParamError('userId'))
+    try {
+      const user = await this.loadAccountById.loadById(httpRequest.params.userId)
+      if (!user) {
+        return forbidden(new InvalidParamError('userId'))
+      }
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    return null
   }
 }
