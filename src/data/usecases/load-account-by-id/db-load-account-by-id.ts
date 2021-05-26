ec8c982b39be0af5a84c9b4d51d8196af38cb4c5
Caio Vieira <contato@caiovieira.com.br>
2021-05-26 10:49:08 -0300

    feat: ensure DbLoadAccountById calls LoadAccountByIdRepository with correct id
    issue: #74

---
+++ src/data/usecases/load-account-by-id/db-load-account-by-id.ts
@@ -0,0 +1,11 @@
+import { LoadAccountByIdRepository } from '@/data/protocols/db/account/load-account-by-id-repository'
+import { AccountModel } from '@/domain/models/account'
+import { LoadAccountById } from '@/domain/usecases/load-account-by-id'
+
+export class DbLoadAccountById implements LoadAccountById {
+  constructor (private readonly loadAccountByIdRepository: LoadAccountByIdRepository) {}
+  async loadById (userId: string): Promise<AccountModel> {
+    await this.loadAccountByIdRepository.loadById(userId)
+    return null
+  }
+}
