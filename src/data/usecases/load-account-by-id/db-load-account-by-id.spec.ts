78b6d6a8d3b8875a428b3ff27b94de084dad860c
Caio Vieira <contato@caiovieira.com.br>
2021-05-26 10:49:55 -0300

    test: ensure DbLoadAccountById calls LoadAccountByIdRepository with correct id
    issue: #74

---
+++ src/data/usecases/load-account-by-id/db-load-account-by-id.spec.ts
@@ -0,0 +1,45 @@
+import { DbLoadAccountById } from './db-load-account-by-id'
+import { LoadAccountByIdRepository } from '@/data/protocols/db/account/load-account-by-id-repository'
+import { AccountModel } from '@/domain/models/account'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_user_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password',
+  role_id: 'valid_role_id'
+})
+
+const makeLoadAccountByIdRepository = (): LoadAccountByIdRepository => {
+  class LoadAccountByIdRepositoryStub implements LoadAccountByIdRepository {
+    async loadById (userId: string): Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+  return new LoadAccountByIdRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbLoadAccountById
+  loadAccountByIdRepositoryStub: LoadAccountByIdRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByIdRepositoryStub = makeLoadAccountByIdRepository()
+  const sut = new DbLoadAccountById(
+    loadAccountByIdRepositoryStub
+  )
+  return {
+    sut,
+    loadAccountByIdRepositoryStub
+  }
+}
+
+describe('DbLoadAccountById Usecase', () => {
+  test('Should call LoadAccountByIdRepository with correct id' , async () => {
+    const { sut, loadAccountByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadAccountByIdRepositoryStub,'loadById')
+    await sut.loadById('valid_user_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('valid_user_id')
+  })
+})
