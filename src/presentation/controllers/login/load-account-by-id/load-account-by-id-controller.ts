d439b002270e1f1888ff6707487d68524e23fc45
Caio Vieira <contato@caiovieira.com.br>
2021-05-26 20:05:55 -0300

    feat: ensure LoadAccountByIdController returns 403 if invalid userId is provided
    issue: #72

--- src/presentation/controllers/login/load-account-by-id/load-account-by-id-controller.ts
+++ src/presentation/controllers/login/load-account-by-id/load-account-by-id-controller.ts
@@ -1,9 +1,14 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadAccountById } from './load-account-by-id-protocols'
 
 export class LoadAccountByIdController implements Controller {
   constructor (private readonly loadAccountById: LoadAccountById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadAccountById.loadById(httpRequest.params.userId)
-    return new Promise(resolve => resolve(null))
+    const user = await this.loadAccountById.loadById(httpRequest.params.userId)
+    if (!user) {
+      return forbidden(new InvalidParamError('userId'))
+    }
+    return null
   }
 }
