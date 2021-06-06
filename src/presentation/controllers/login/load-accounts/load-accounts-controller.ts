ca464226ebe4e84d6679c0b207b4193c5d88ac68
Caio Vieira <contato@caiovieira.com.br>
2021-06-06 08:35:01 -0300

    feat: ensure LoadAccountsController returns 200 on success
    issue: #92

--- src/presentation/controllers/login/load-accounts/load-accounts-controller.ts
+++ src/presentation/controllers/login/load-accounts/load-accounts-controller.ts
@@ -1,12 +1,12 @@
 import { Controller, HttpRequest, HttpResponse, LoadAccounts } from './load-accounts-protocols'
-import { serverError } from '@/presentation/helpers/http/http-helper'
+import { ok, serverError } from '@/presentation/helpers/http/http-helper'
 
 export class LoadAccountsController implements Controller {
   constructor (private readonly loadAccounts: LoadAccounts) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadAccounts.load()
-      return Promise.resolve(null)
+      const accounts = await this.loadAccounts.load()
+      return ok(accounts)
     } catch (error) {
       return serverError(error)
     }
