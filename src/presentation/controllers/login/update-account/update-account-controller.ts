2c87eae881d0453148c56df440d3cb9539f12d25
Caio Vieira <contato@caiovieira.com.br>
2021-06-16 19:29:37 -0300

    feat: ensure UpdateAccountController returns 400 if validation fails
    issue: #109

--- src/presentation/controllers/login/update-account/update-account-controller.ts
+++ src/presentation/controllers/login/update-account/update-account-controller.ts
@@ -1,9 +1,13 @@
 import { Controller,HttpRequest, HttpResponse, Validation } from './update-account-protocols'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 export class UpdateAccountController implements Controller {
   constructor (private readonly validation: Validation) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return Promise.resolve(null)
   }
 }
