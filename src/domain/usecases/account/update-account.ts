81ca75b05205545b76145c01f2832491a61a9f91
Caio Vieira <contato@caiovieira.com.br>
2021-06-17 19:49:41 -0300

    refactor: put id on UpdateAccountParams
    issue: #116

--- src/domain/usecases/account/update-account.ts
+++ src/domain/usecases/account/update-account.ts
@@ -1,6 +1,6 @@
 import { AccountModel } from '@/domain/models/account'
 
-export type UpdateAccountParams = Omit<AccountModel, 'id'| 'password'>
+export type UpdateAccountParams = Omit<AccountModel, 'password'>
 
 export interface UpdateAccount {
   update: (account: UpdateAccountParams) => Promise<AccountModel>
