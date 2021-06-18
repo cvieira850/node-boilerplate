9133f4bc9f668c7447f2a79c054752ed9d22314f
Caio Vieira <contato@caiovieira.com.br>
2021-06-18 19:54:37 -0300

    feat: ensure DbUpdateAccount returns an account on UpdateAccountRepository Usecase success
    issue: #110

--- src/data/usecases/account/update-account/db-update-account.ts
+++ src/data/usecases/account/update-account/db-update-account.ts
@@ -3,7 +3,7 @@ import { AccountModel, UpdateAccount, UpdateAccountParams, UpdateAccountReposito
 export class DbUpdateAccount implements UpdateAccount {
   constructor (private readonly updateAccountRepository: UpdateAccountRepository) {}
   async update (account: UpdateAccountParams): Promise<AccountModel> {
-    await this.updateAccountRepository.update(account)
-    return Promise.resolve(null)
+    const updatedAccount = await this.updateAccountRepository.update(account)
+    return updatedAccount
   }
 }
